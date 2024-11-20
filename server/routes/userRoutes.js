aaa4e0b763077cbec0c063d710e793518a119357
P04U <dbsehdwns1@gmail.com>
2024-11-21 00:16:41 +0900

    사용자 프로필 관련 라우트 주석 처리: 관심사 관련 API 엔드포인트 비활성화

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,8 +8,8 @@ router.post('/login', userController.login);
 router.delete('/delete', userController.deleteUser);
 router.put('/:userId/profile', userController.addProfile);
 
-router.post('/:userId/profiles/:profileId/interests', userController.addInterest);
-router.put('/:userId/profiles/:profileId/interests/:interestId', userController.updateInterest);
-router.get('/:userId/profiles/:profileId/interests', userController.getInterests);
+// router.post('/:userId/profiles/:profileId/interests', userController.addInterest);
+// router.put('/:userId/profiles/:profileId/interests/:interestId', userController.updateInterest);
+// router.get('/:userId/profiles/:profileId/interests', userController.getInterests);
 
 module.exports = router;
