66a33af461df76181c5e5094fcb0274a95d973ec
P04U <dbsehdwns1@gmail.com>
2024-12-27 14:27:17 +0900

    MongoDB 연결 로직 수정 및 라우터 경로 변경

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 // app.js
 require('dotenv').config();
 const express = require('express');
-const { default: mongoose } = require('mongoose');
+const { default: mongoose, connect } = require('mongoose');
 const cors = require('cors');
 const loggingMiddleware = require('./middlewares/logging'); // 미들웨어 가져오기
 const userRouter = require('./routes/userRoutes');
@@ -9,30 +9,22 @@ const postRouter = require('./routes/postRoutes');
 const auctionRouter = require('./routes/auctionRoutes');
 const chatRouter = require('./routes/chatRoutes');
 const authRouter = require('./routes/authRoutes');
+const connectDB = require('./config/mongoose');
 
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 // app.set('trust proxy', 1); 'trust proxy'를 1로 설정하면 클라이언트의 IP 주소를 신뢰합니다.
 app.set('trust proxy', 1);
-
-const MONGO_URI = 'mongodb+srv://p04u:VpJL4ftrjy7RbWO6@cluster0.0nr56.mongodb.net/';
 
 if (process.env.NODE_ENV !== 'production') {
   mongoose.set('debug', true); // 몽고 쿼리가 콘솔에서 뜨게 한다.
 }
 
-
 // MongoDB 연결
-const options = {
-  autoIndex: false, // Don't build indexes
-  serverSelectionTimeoutMS: 60000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
-}
-mongoose.connect(MONGO_URI, options)
-// .then(() => console.log('MongoDB connected!'))
-//   .catch((err) => console.log('MongoDB connection error: ', err))
-  ;
+connectDB();
 
 // CORS 미들웨어 적용
 app.use(cors());
@@ -45,8 +37,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(loggingMiddleware);
 
 // 라우터 등록
-app.use('/user', userRouter);
-app.use('/post', postRouter);
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
 app.use('/auctions', auctionRouter);
 app.use('/chat', chatRouter);
 app.use('/auth', authRouter);
@@ -54,9 +46,5 @@ app.use('/auth', authRouter);
 app.get('/', (req, res) => {
   res.send('Hello, World! \n This is the main page of the app');
 });
-
-// app.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
 
 module.exports = app;
