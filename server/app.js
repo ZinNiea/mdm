fbddf51ba16cecbb978b563ac4b8d86e8611ec32
P04U <dbsehdwns1@gmail.com>
2025-01-03 13:19:03 +0900

    Basic 인증 및 IP 화이트리스트 미들웨어 추가, 게시물 라우트 경로 수정

--- server/app.js
+++ server/app.js
@@ -16,6 +16,42 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerSpec = require('./docs/swaggerOptions');
+const swaggerAllowedIPs = [];
+
+// Basic 인증 미들웨어
+const basicAuth = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  
+  if (!authHeader) {
+    res.setHeader('WWW-Authenticate', 'Basic realm="401"');
+    return res.status(401).send('인증이 필요합니다.');
+  }
+
+  const base64Credentials = authHeader.split(' ')[1];
+  const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+  const [username, password] = credentials.split(':');
+
+  const validUser = process.env.SWAGGER_USER;
+  const validPass = process.env.SWAGGER_PASSWORD;
+
+  if (username === validUser && password === validPass) {
+    return next();
+  }
+
+  res.setHeader('WWW-Authenticate', 'Basic realm="401"');
+  return res.status(401).send('인증이 필요합니다.');
+};
+
+// IP 화이트리스트 미들웨어
+const ipWhitelist = (req, res, next) => {
+  const clientIP = req.ip;
+
+  if (allowedIPs.includes(clientIP)) {
+    return next();
+  } else {
+    return res.status(403).send('접근이 금지되었습니다.');
+  }
+};
 
 // app.set('trust proxy', 1); 'trust proxy'를 1로 설정하면 클라이언트의 IP 주소를 신뢰합니다.
 app.set('trust proxy', 1);
@@ -27,7 +63,7 @@ if (process.env.NODE_ENV !== 'production') {
 // MongoDB 연결
 connectDB();
 
-// Swagger 미들웨어 적용
+// Swagger 미들웨어 적용 전에 IP 화이트리스트와 Basic Auth 미들웨어 추가
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // CORS 미들웨어 적용
@@ -41,8 +77,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(loggingMiddleware);
 
 // 라우터 등록
-app.use('/users', userRouter);
-app.use('/posts', postRouter);
+app.use('/user', userRouter);
+app.use('/post', postRouter);
 app.use('/auctions', auctionRouter);
 app.use('/chat', chatRouter);
 app.use('/auth', authRouter);
